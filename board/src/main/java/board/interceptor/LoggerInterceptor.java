92273557d76d62c63d574f41107abbbe3a40fc26
UMK <haga5032@naver.com>
2023-02-02 13:54:27 +0900

    106p

---
+++ board/src/main/java/board/interceptor/LoggerInterceptor.java
@@ -0,0 +1,31 @@
+package board.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggerInterceptor  implements HandlerInterceptor{
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.debug("======================================          START         ======================================");
+		log.debug(" Request URI \t:  " + request.getRequestURI());
+//		return super.preHandle(request, response, handler);
+		return true;
+		
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		log.debug("======================================           END          ======================================\n");
+	}
+}
+
